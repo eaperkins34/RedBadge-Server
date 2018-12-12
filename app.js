df62d9b231c2afe7e341cec5c21270e0f9d44f72
Emily Perkins <eaperkins34@gmail.com>
2018-12-12 11:10:46 -0500

    push server

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 require('dotenv').config();
 
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const sequelize = require('./db');
+
+
+// sequelize.sync({force: true});
+sequelize.sync()
+
+app.use(bodyParser.json())
+app.use(require ('./middleware/headers'))
 
 require('./models')
 
 app.use('/business', require('./controllers/business-controller'))
 app.use('/theme', require('./controllers/theme-controller'))
 
-app.use(bodyParser.json())
 
-// sequelize.sync();
-sequelize.sync()
 
 app.listen(process.env.PORT, () => {
     console.log(`server is listening on port: ${process.env.PORT}`)
